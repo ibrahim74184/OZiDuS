2ebca1f33f7264141aa8372422907f423a191d6d
Şenol IRMAK <senolirmak@gmail.com>
2020-12-09 00:21:18 +0300

    Update oziliuret.py

--- ozidusproject/scripts/oziliuret.py
+++ ozidusproject/scripts/oziliuret.py
@@ -1,9 +1,9 @@
 from django.db import models
-"""from ozildata.models import ZilMelodi
+"""from ozildata.models import ZilData
 
 
 def uret():
-    gziller = ZilMelodi.objects.all.filter(active=True)
+    gziller = ZilData.objects.all.filter(active=True)
     for i in range(len(gziller)):
         for x, y in gziller[i].items():
             print(f"{x}, {y}")
