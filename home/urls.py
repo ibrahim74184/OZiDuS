ac4274d2a4d9881917f9affbd0dde35e13ad9b9c
senolirmak <senolirmak@gmail.com>
2020-12-10 21:27:07 +0300

    düzenleme

--- home/urls.py
+++ home/urls.py
@@ -1,1 +1,7 @@
 from django.urls import path
+from home.views import home_views
+
+urlpatterns = [
+    path('', home_views, name='home'),
+
+]
--- home/views.py
+++ home/views.py
@@ -1,2 +1,6 @@
-from django.shortcuts import render
-#Baktim
+from django.shortcuts import render, redirect, get_object_or_404
+
+
+def home_views(request):
+    return render(request, 'home/index.html')
+
